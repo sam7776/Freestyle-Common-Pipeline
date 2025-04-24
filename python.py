f1d986ee9c7cddc8634e45c1a25dacfe422eb1fb
Nisahnt <43927210+sam7776@users.noreply.github.com>
2025-04-24 18:38:43 +0530

    Update python.py
    updated

--- python.py
+++ python.py
@@ -1,35 +1,35 @@
-def print_sonu():
+def print_stars():
     s = [
-        " SSS ",
-        "S     ",
-        " SSS ",
-        "    S",
-        " SSS "
+        " *** ",
+        "*    ",
+        " *** ",
+        "    *",
+        " *** "
     ]
     o = [
-        " OOO ",
-        "O   O",
-        "O   O",
-        "O   O",
-        " OOO "
+        " *** ",
+        "*   *",
+        "*   *",
+        "*   *",
+        " *** "
     ]
     n = [
-        "N   N",
-        "NN  N",
-        "N N N",
-        "N  NN",
-        "N   N"
+        "*   *",
+        "**  *",
+        "* * *",
+        "*  **",
+        "*   *"
     ]
     u = [
-        "U   U",
-        "U   U",
-        "U   U",
-        "U   U",
-        " UUU "
+        "*   *",
+        "*   *",
+        "*   *",
+        "*   *",
+        " *** "
     ]
 
     # Combine all letters row by row
     for row in range(5):
         print(s[row] + "  " + o[row] + "  " + n[row] + "  " + u[row])
 
-print_sonu()
+print_stars()
